3d5deca52da2b05ede3239f7041835be91edded2
Rosalito gabriel <rosalitogabriel@solaireresort.com>
2024-01-26 04:51:02 +0800

    Added EnvVars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
+import { config } from 'dotenv';
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { HealthModule } from './health/health.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [TasksModule, HealthModule],
+  imports: [
+    TasksModule,
+    HealthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: `${process.env.PG_ENDPOINT}`,
+      port: 5432,
+      username: `${process.env.PG_USERNAME}`,
+      password: `${process.env.PG_PASSWORD}`,
+      database: `${process.env.PG_DATABASE}`,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
